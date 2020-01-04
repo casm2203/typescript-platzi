768adef43176ba7e4eb6f8e417cc393743857380
Cristian Suarez <cristian.suarez0322@gmail.com>
2020-01-03 23:58:39 -0500

    configuracionFirebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyB_RC0scje2K8irFjLOxr8ouxCTYoVp3BA',
+    authDomain: 'typescrip-platzi-e8c57.firebaseapp.com',
+    databaseURL: 'https://typescrip-platzi-e8c57.firebaseio.com',
+    projectId: 'typescrip-platzi-e8c57',
+    storageBucket: 'typescrip-platzi-e8c57.appspot.com',
+    messagingSenderId: '490537127131',
+    appId: '1:490537127131:web:662cba5a48be9c061710fe',
+    measurementId: 'G-7F7GQQQ7KH'
+  }
 };
 
 /*
